5a5bdf3c60029db3d7e71aa3ec8ea0eb0324d144
messicy <1010985950@qq.com>
2020-01-17 10:16:24 +0800

    修改mq相关

--- src/main/java/com/messiyang/idempotency/config/RabbitConfig.java
+++ src/main/java/com/messiyang/idempotency/config/RabbitConfig.java
@@ -91,11 +91,4 @@ public class RabbitConfig {
         return BindingBuilder.bind(mailQueue()).to(mailExchange()).with(MAIL_ROUTING_KEY_NAME);
     }
 
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        return factory;
-    }
 }
